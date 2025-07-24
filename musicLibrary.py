f85fc5170f29ad10cfd122db98f846f55a2d5f38
aashika-08 <aashikajain0726@gmail.com>
2025-07-24 16:47:00 +0530

    Uploading files

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "agar": "https://www.youtube.com/watch?v=sK7riqg2mr4",
+    "satranga": "https://www.youtube.com/watch?v=HrnrqYxYrbk",
+    "sauda": "https://www.youtube.com/watch?v=CxAWKewvooo",
+    "bulleya": "https://www.youtube.com/watch?v=a9Hxkc9YxGE",
+    "man": "https://www.youtube.com/watch?v=fXRvluHnjxE",
+    "pal": "https://www.youtube.com/watch?v=nDOpAEW2q2w",
+}
